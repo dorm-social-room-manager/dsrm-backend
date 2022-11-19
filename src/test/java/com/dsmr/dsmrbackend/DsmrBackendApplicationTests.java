f504f43250c37a14256350c95885e2a7a40a016d
krizzu <kskrzysiek788@gmail.com>
2022-11-19 11:06:04 +0100

    Init

---
+++ src/test/java/com/dsmr/dsmrbackend/DsmrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsmr.dsmrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmrBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
