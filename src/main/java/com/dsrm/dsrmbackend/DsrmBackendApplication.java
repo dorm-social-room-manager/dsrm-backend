4e0e57dc490cc8b7ea848aa3f706ca1aa02ff447
krizzu <kskrzysiek788@gmail.com>
2022-11-19 11:18:39 +0100

    Spring Init

---
+++ src/main/java/com/dsrm/dsrmbackend/DsrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dsrm.dsrmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsrmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsrmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dsrm/dsrmbackend/DsrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsrm.dsrmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsrmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
