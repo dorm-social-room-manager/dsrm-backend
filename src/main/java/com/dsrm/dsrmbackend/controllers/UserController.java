3dd6ae348ec3bd2de68390a1a01dec70dcb2a48a
Adrian-Rochminski <235959@edu.p.lodz.pl>
2022-12-11 19:04:07 +0100

    fix: Create-rest-user-endpoints-to-retrieve-specific-user-and-all-users
    closses issue #7

--- src/main/java/com/dsrm/dsrmbackend/controllers/UserController.java
+++ src/main/java/com/dsrm/dsrmbackend/controllers/UserController.java
@@ -6,11 +6,10 @@ import com.dsrm.dsrmbackend.dto.UserDTO;
 import com.dsrm.dsrmbackend.dto.UserRequestDTO;
 import com.dsrm.dsrmbackend.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,19 +19,12 @@ public class UserController {
     private final UserService userService;
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping(value = "/users", consumes ="application/json")
+    @PostMapping(value = "/users", consumes= MediaType.APPLICATION_JSON_VALUE)
     void addUser(@RequestBody UserRequestDTO userRequestDTO) {}
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(value = "/users/{id}", produces ="application/json", consumes ="application/json")
-    ResponseEntity<UserDTO> getUser(@PathVariable Long id) throws JSONException {
-        JSONObject outputObj = new JSONObject();
-        outputObj.put("email",null);
-        outputObj.put("name",null);
-        outputObj.put("surname",null);
-        outputObj.put("roles",null);
-        outputObj.put("isBanned",null);
-        outputObj.put("banEndDate",null);
+    @GetMapping(value = "/users/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes =MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<UserDTO> getUser(@PathVariable Long id){
         return null;
     }
 
