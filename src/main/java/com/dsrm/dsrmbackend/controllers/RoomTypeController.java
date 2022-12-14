eaac1898539aebb85bf1235785e48113615cf516
Boomber863 <boomber8634@gmail.com>
2022-12-14 16:33:13 +0100

    feat: Create rest endpoint to add new room type

---
+++ src/main/java/com/dsrm/dsrmbackend/controllers/RoomTypeController.java
@@ -0,0 +1,20 @@
+package com.dsrm.dsrmbackend.controllers;
+
+import com.dsrm.dsrmbackend.dto.RoomTypeRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RoomTypeController {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/room-types", consumes= MediaType.APPLICATION_JSON_VALUE)
+    void addRoomType(@RequestBody RoomTypeRequestDTO roomTypeRequestDTO) {}
+
+}
---
+++ src/main/java/com/dsrm/dsrmbackend/dto/RoomTypeRequestDTO.java
@@ -0,0 +1,13 @@
+package com.dsrm.dsrmbackend.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class RoomTypeRequestDTO {
+    private String name;
+}
