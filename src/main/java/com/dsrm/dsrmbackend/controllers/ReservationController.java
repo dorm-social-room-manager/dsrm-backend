47bf00cc101e9b367a253b4034fe6bca1a3a2a5c
Boomber863 <boomber8634@gmail.com>
2022-12-14 16:41:15 +0100

    feat: Create rest endpoint to add new reservation

---
+++ src/main/java/com/dsrm/dsrmbackend/controllers/ReservationController.java
@@ -0,0 +1,20 @@
+package com.dsrm.dsrmbackend.controllers;
+
+import com.dsrm.dsrmbackend.dto.ReservationRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ReservationController {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/reservations", consumes= MediaType.APPLICATION_JSON_VALUE)
+    void addReservation(@RequestBody ReservationRequestDTO reservationRequestDTO) {}
+
+}
---
+++ src/main/java/com/dsrm/dsrmbackend/dto/ReservationRequestDTO.java
@@ -0,0 +1,18 @@
+package com.dsrm.dsrmbackend.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class ReservationRequestDTO {
+    private long room;
+    private LocalDateTime openingTime;
+    private LocalDateTime closingTime;
+    private long user;
+}
