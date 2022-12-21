302932f2447c7aae5126cd08e1e598dcc9174f5b
Krzych <kskrzysiek788@gmail.com>
2022-12-21 14:08:49 +0100

    feat: attempt to fix RoomTypeMapper

--- src/main/java/com/dsrm/dsrmbackend/mappers/RoomTypeMapper.java
+++ src/main/java/com/dsrm/dsrmbackend/mappers/RoomTypeMapper.java
@@ -3,10 +3,15 @@ package com.dsrm.dsrmbackend.mappers;
 import com.dsrm.dsrmbackend.dto.RoomTypeDTO;
 import com.dsrm.dsrmbackend.dto.RoomTypeRequestDTO;
 import com.dsrm.dsrmbackend.entities.RoomType;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+@Mapper(componentModel = "spring")
 public interface RoomTypeMapper {
-    RoomTypeMapper INSTANCE = Mappers.getMapper(RoomTypeMapper.class);
+    public RoomTypeMapper INSTANCE = Mappers.getMapper(RoomTypeMapper.class);
+    @Mapping(target = "id", ignore = true)
     RoomType RoomTypeReqDTOToRoomType(RoomTypeRequestDTO roomTypeReqDto);
+
     RoomTypeDTO RoomTypeToRoomTypeDTO(RoomType roomType);
 }
