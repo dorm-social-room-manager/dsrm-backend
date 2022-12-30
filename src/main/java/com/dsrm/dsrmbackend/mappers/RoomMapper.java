6841ab38bc423d7645919a7152075a580c3a3c34
Krzych <kskrzysiek788@gmail.com>
2022-12-30 15:46:29 +0100

    feat: added additional mapping of room type

--- src/main/java/com/dsrm/dsrmbackend/mappers/RoomMapper.java
+++ src/main/java/com/dsrm/dsrmbackend/mappers/RoomMapper.java
@@ -3,6 +3,7 @@ package com.dsrm.dsrmbackend.mappers;
 import com.dsrm.dsrmbackend.dto.RoomDTO;
 import com.dsrm.dsrmbackend.dto.RoomRequestDTO;
 import com.dsrm.dsrmbackend.entities.Room;
+import com.dsrm.dsrmbackend.entities.RoomType;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -10,7 +11,8 @@ import org.mapstruct.Mapping;
 public interface RoomMapper {
 
     @Mapping(source = "number", target = "roomNumber")
+    @Mapping(source = "type", target = "roomType")
     Room roomReqDTOToRoom(RoomRequestDTO roomDto);
+    RoomType map(Long id);
     RoomDTO roomToRoomDTO(Room room);
-
 }
