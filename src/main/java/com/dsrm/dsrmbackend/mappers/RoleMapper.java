f650f7ee36b81f9bac49bec882912d2424e8c9ce
Jan Gnatkowski <71210407+Boomber863@users.noreply.github.com>
2023-02-05 23:51:45 +0100

    25 Create role service (#45)
    * feat: Create role service
    
    * fix: fixed annotations and indents

--- src/main/java/com/dsrm/dsrmbackend/mappers/RoleMapper.java
+++ src/main/java/com/dsrm/dsrmbackend/mappers/RoleMapper.java
@@ -1,18 +1,23 @@
 package com.dsrm.dsrmbackend.mappers;
 
+import com.dsrm.dsrmbackend.dto.RoleDTO;
+import com.dsrm.dsrmbackend.dto.RoleRequestDTO;
 import com.dsrm.dsrmbackend.entities.Role;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 import java.util.Set;
 
 @Mapper(componentModel = "spring")
 public interface RoleMapper {
+    Role roleReqDTOToRole(RoleRequestDTO roleReqDto);
+
+    RoleDTO roleToRoleDTO(Role role);
 
     List<Role> toRolesMap(List<Long> id);
 
     Set<Role> toSetMap(List<Role> list);
 
     Role toRole(Long id);
-
 }
