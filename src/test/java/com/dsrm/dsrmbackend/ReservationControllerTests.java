578c29bc24b94dc63f64367981f589ca96f8e7bf
Adrian-Rochminski <235959@edu.p.lodz.pl>
2023-02-06 22:30:17 +0100

    fix:  create reservation service

--- src/test/java/com/dsrm/dsrmbackend/ReservationControllerTests.java
+++ src/test/java/com/dsrm/dsrmbackend/ReservationControllerTests.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,6 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(initializers = {AbstractIntegrationTest.DatabaseInitializer.class})
 @AutoConfigureMockMvc
+@DirtiesContext
 class ReservationControllerTests  extends  AbstractIntegrationTest{
     @Autowired
     private MockMvc mockMvc;
