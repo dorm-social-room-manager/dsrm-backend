22192899de9373387138c3b4301590562ae22fc2
Adrian-Rochminski <79656651+Adrian-Rochminski@users.noreply.github.com>
2023-02-18 23:34:14 +0100

    feat: Add validation to AddUser and createReservation endpoint (#62)

--- src/main/java/com/dsrm/dsrmbackend/dto/UserRequestDTO.java
+++ src/main/java/com/dsrm/dsrmbackend/dto/UserRequestDTO.java
@@ -4,15 +4,20 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Getter
 @Setter
 public class UserRequestDTO {
+    @NotBlank
     private String email;
+    @NotBlank
     private String password;
+    @NotBlank
     private String name;
+    @NotBlank
     private String surname;
     private List<Long> roles;
 }
