8eaf7f6cfbe036db25ffdcf8ddc0b7667cf9adb7
Krzych <kskrzysiek788@gmail.com>
2023-03-01 18:58:11 +0100

    fix: updating room type if chosen type exists

--- src/main/java/com/dsrm/dsrmbackend/services/impl/RoomServiceImpl.java
+++ src/main/java/com/dsrm/dsrmbackend/services/impl/RoomServiceImpl.java
@@ -2,9 +2,10 @@ package com.dsrm.dsrmbackend.services.impl;
 
 import com.dsrm.dsrmbackend.dto.RoomRequestDTO;
 import com.dsrm.dsrmbackend.entities.Room;
+import com.dsrm.dsrmbackend.entities.RoomType;
 import com.dsrm.dsrmbackend.mappers.RoomMapper;
-import com.dsrm.dsrmbackend.mappers.RoomTypeMapper;
 import com.dsrm.dsrmbackend.repositories.RoomRepo;
+import com.dsrm.dsrmbackend.repositories.RoomTypeRepo;
 import com.dsrm.dsrmbackend.services.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -17,32 +18,35 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class RoomServiceImpl implements RoomService {
-    private final RoomRepo repository;
+    private final RoomRepo roomRepo;
+    private final RoomTypeRepo roomTypeRepo;
     private final RoomMapper roomMapper;
-    private final RoomTypeMapper roomTypeMapper;
     public Room addRoom(RoomRequestDTO roomDto) {
         Room room = roomMapper.roomReqDTOToRoom(roomDto);
         room.setId(UUID.randomUUID().toString());
-        return repository.save(room);
+        return roomRepo.save(room);
     }
 
     public Optional<Room> getRoom(String roomId) {
-        return repository.findById(roomId);
+        return roomRepo.findById(roomId);
     }
 
     public Page<Room> getRooms(Pageable pageable) {
-        return repository.findAll(pageable);
+        return roomRepo.findAll(pageable);
     }
 
     @Override
     public Room updateRoom(RoomRequestDTO update, String roomId) {
-        Optional<Room> room = repository.findById(roomId);
+        Optional<Room> room = roomRepo.findById(roomId);
         Room newRoom;
         if (room.isPresent()) {
             newRoom = room.get();
             newRoom.setRoomNumber(update.getNumber());
             newRoom.setFloor(update.getFloor());
-            newRoom.setRoomType(roomTypeMapper.mapIdToRoomType(update.getType()));
+            Optional<RoomType> roomType = roomTypeRepo.findById(update.getType());
+            if (newRoom.getRoomType() != null && roomType.isPresent()) {
+                newRoom.setRoomType(roomType.get());
+            }
             newRoom.setMaxCapacity(update.getMaxCapacity());
             newRoom.setOpeningTime(update.getOpeningTime());
             newRoom.setClosingTime(update.getClosingTime());
@@ -51,6 +55,6 @@ public class RoomServiceImpl implements RoomService {
             newRoom = roomMapper.roomReqDTOToRoom(update);
             newRoom.setId(roomId);
         }
-        return repository.save(newRoom);
+        return roomRepo.save(newRoom);
     }
 }
