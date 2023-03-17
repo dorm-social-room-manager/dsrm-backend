b9a688c05b8165842b3a6c9f08777b4f6b07c860
Krzych <kskrzysiek788@gmail.com>
2023-03-17 17:10:15 +0100

    feat: added method to get room list with specific room type

--- src/main/java/com/dsrm/dsrmbackend/repositories/RoomRepo.java
+++ src/main/java/com/dsrm/dsrmbackend/repositories/RoomRepo.java
@@ -1,7 +1,11 @@
 package com.dsrm.dsrmbackend.repositories;
 
 import com.dsrm.dsrmbackend.entities.Room;
+import com.dsrm.dsrmbackend.entities.RoomType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomRepo extends JpaRepository<Room,String> {
+    List<Room> getRoomsByRoomType(RoomType roomType);
 }
