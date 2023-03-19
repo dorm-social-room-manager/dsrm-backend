0863454fbcdf86c6ee04e90ff8a21154082570cd
Krzych <kskrzysiek788@gmail.com>
2023-03-19 19:19:35 +0100

    feat: added @NotBlank annotation to name

--- src/main/java/com/dsrm/dsrmbackend/dto/RoleRequestDTO.java
+++ src/main/java/com/dsrm/dsrmbackend/dto/RoleRequestDTO.java
@@ -1,12 +1,14 @@
 package com.dsrm.dsrmbackend.dto;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
 
 
 @Getter
 @Setter
 public class RoleRequestDTO {
+    @NotBlank
     private String name;
 }
