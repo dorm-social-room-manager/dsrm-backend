11e01d8de0090350fcfcdd17651d0026505c3db3
Krzych <kskrzysiek788@gmail.com>
2023-05-05 23:19:02 +0200

    fix: fixed relationship problem

--- src/main/java/com/dsrm/dsrmbackend/entities/RoomType.java
+++ src/main/java/com/dsrm/dsrmbackend/entities/RoomType.java
@@ -1,5 +1,6 @@
 package com.dsrm.dsrmbackend.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ public class RoomType {
     @Id
     private String id;
     private String name;
+    @JsonIgnore
     @OneToMany(mappedBy = "roomType", cascade = CascadeType.REMOVE)
     private List<Room> rooms;
 }
