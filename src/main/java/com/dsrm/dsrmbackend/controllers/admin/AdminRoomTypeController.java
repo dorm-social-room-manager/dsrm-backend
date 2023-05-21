3a96fba0147950aedf9b484bdf754e78d3634564
krzych-ubuntu <kskrzysiek788@gmail.com>
2023-05-21 13:58:37 +0200

    refactor: fixed method name

--- src/main/java/com/dsrm/dsrmbackend/controllers/admin/AdminRoomTypeController.java
+++ src/main/java/com/dsrm/dsrmbackend/controllers/admin/AdminRoomTypeController.java
@@ -51,7 +51,7 @@ public class AdminRoomTypeController {
     }
 
     @DeleteMapping(value="/room-types/{id}")
-    public ResponseEntity<RoomType> deleteRoom(@PathVariable String id) {
+    public ResponseEntity<RoomType> deleteRoomType(@PathVariable String id) {
         Optional<RoomType> roomType = roomTypeService.deleteRoomType(id);
         return roomType.map(type -> new ResponseEntity<>(type, HttpStatus.NO_CONTENT))
                 .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
