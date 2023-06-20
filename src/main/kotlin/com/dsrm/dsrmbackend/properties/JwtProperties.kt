e803d747d86567ace7bc6bfcbe1adb6e01b972f4
Krizzuu <kskrzysiek788@gmail.com>
2023-06-20 10:28:31 +0200

    feat: changed JwtProperties to data class

--- src/main/kotlin/com/dsrm/dsrmbackend/properties/JwtProperties.kt
+++ src/main/kotlin/com/dsrm/dsrmbackend/properties/JwtProperties.kt
@@ -1,14 +1,14 @@
 package com.dsrm.dsrmbackend.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import java.security.Key
 
 
 @ConfigurationProperties(prefix = "dsrm.auth.jwt")
-class JwtProperties {
-    var expirationMs = 0
-
-    var refreshExpirationMs = 0
-
+@ConstructorBinding
+data class JwtProperties (
+    var expirationMs: Int = 0,
+    var refreshExpirationMs: Int = 0,
     var secretKey: Key? = null
-}
+)
